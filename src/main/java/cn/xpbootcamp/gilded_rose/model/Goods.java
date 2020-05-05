0e607830b57a75cff7d103e8bf94af53e785a6b3
jiangze.mei <jiangze.mei@thoughtworks.com>
2020-05-05 16:59:08 +0800

    [Jiangze] create Goods class with two properties and Goods type enum with four different types

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/model/Goods.java
@@ -0,0 +1,16 @@
+package cn.xpbootcamp.gilded_rose.model;
+
+public class Goods {
+
+  private int sellIn;
+
+  private int quality;
+
+  public int getSellIn() {
+    return sellIn;
+  }
+
+  public void setSellIn(int sellIn) {
+    this.sellIn = sellIn;
+  }
+}
---
+++ src/main/java/cn/xpbootcamp/gilded_rose/model/GoodsType.java
@@ -0,0 +1,8 @@
+package cn.xpbootcamp.gilded_rose.model;
+
+public enum GoodsType {
+  NORMAL,
+  AGED_BRIE,
+  SULFURAS,
+  BACKSTAGE_PASS
+}
