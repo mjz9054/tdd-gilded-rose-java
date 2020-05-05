cc90d8ec7886d4fb3d58a5ce3a399e4f861b453c
jiangze.mei <jiangze.mei@thoughtworks.com>
2020-05-05 18:04:52 +0800

    [Jiangze] declare max and min quality in GoodsQualityCalculateAlgorithm interface

--- src/main/java/cn/xpbootcamp/gilded_rose/algorithm/GoodsQualityCalculateAlgorithm.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/algorithm/GoodsQualityCalculateAlgorithm.java
@@ -2,7 +2,10 @@ package cn.xpbootcamp.gilded_rose.algorithm;
 
 import cn.xpbootcamp.gilded_rose.model.Goods;
 
-interface GoodsQualityCalculateAlgorithm {
+public interface GoodsQualityCalculateAlgorithm {
+
+  int MIN_QUALITY = 0;
+  int MAX_QUALITY = 50;
 
   int calculate(Goods goods, int passedDays);
 }
