8760a673088b76d7fd1a8790b14c8fff118a9d32
jiangze.mei <jiangze.mei@thoughtworks.com>
2020-05-05 17:05:41 +0800

    [Jiangze] create a interface for quality calculation with a `calculate` function

---
+++ src/main/java/cn/xpbootcamp/gilded_rose/algorithm/GoodsQualityCalculateAlgorithm.java
@@ -0,0 +1,8 @@
+package cn.xpbootcamp.gilded_rose.algorithm;
+
+import cn.xpbootcamp.gilded_rose.model.Goods;
+
+interface GoodsQualityCalculateAlgorithm {
+
+  int calculate(Goods goods, int passedDays);
+}
